82bc5ed4f49a14bb7ed93e6cfa4b99c5521901aa
yuanjing-sudo <yuan_jing@students.edu.sg>
2026-07-04 10:13:40 +0800

    Initial Commit

---
+++ 67/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  67
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 67/_7App.swift
@@ -0,0 +1,17 @@
+//
+//  _7App.swift
+//  67
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct _7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
